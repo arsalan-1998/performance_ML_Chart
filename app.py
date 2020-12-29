16cc49123410414fe0fd980edfd057bc1cac54f0
De_Codr <35762748+arsalan-1998@users.noreply.github.com>
2020-12-29 23:14:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/', methods = ['POST', 'GET'])
+def singleGraph():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/compare')
+def compareGraph():
+    if request.method == 'GET':
+        return render_template('compare.html')
